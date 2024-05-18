359e503e5ee887200cca1ce93d7acdb1b2454106
Dylan Foster <fosterdill@gmail.com>
2024-05-18 03:05:42 -0700

    add postgres

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,11 @@
-export async function GET() {
-  const res = await fetch('https://dummyjson.com/products');
-  const data = await res.json();
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-  return Response.json({ data });
+export async function GET(request: Request) {
+  try {
+    const result = await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
