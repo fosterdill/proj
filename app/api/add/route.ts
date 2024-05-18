0ff7a337b48336e51b4ca39c0e7ef0477140bfe5
Dylan Foster <fosterdill@gmail.com>
2024-05-18 03:07:20 -0700

    fix linter

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -7,7 +7,9 @@ export async function GET(request: Request) {
   const ownerName = searchParams.get('ownerName');
 
   try {
-    if (!petName || !ownerName) throw new Error('Pet and owner names required');
+    if (!petName || !ownerName) {
+      throw new Error('Pet and owner names required');
+    }
     await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
