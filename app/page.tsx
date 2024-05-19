f192e34d0e853df196c4d2a4b8021bebd5033920
Dylan Foster <fosterdill@gmail.com>
2024-05-18 21:42:09 -0700

    testing server side db query

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
-'use client';
 import { List, ListItem } from '@chakra-ui/react';
+import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { useEffect, useState } from 'react';
 
-import { User } from '@/schema';
+import { users } from '@/schema';
 
-export default function Home() {
-  const [users, setUsers] = useState<{ result: User[] }>({ result: [] });
+const db = drizzle(sql);
 
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(`/api/users`);
-      const users: { result: User[] } = await res.json();
-
-      setUsers(users);
-    })();
-  }, []);
-
+export default async function Home() {
+  const result = await db.select().from(users);
   return (
     <List>
-      {users.result.map((user: User) => {
+      {result.map((user) => {
         return <ListItem key={user.id}>{user.firstName}</ListItem>;
       })}
     </List>
