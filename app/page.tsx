88de1f7b935d0372ea204491d19453d46d248020
Dylan Foster <fosterdill@gmail.com>
2024-05-18 21:17:55 -0700

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import baseUrl from '@/baseUrl';
 import { User } from '@/schema';
 
 export default async function Home() {
-  const res = await fetch(`${baseUrl}/api/users`);
-  const users = (await res.json()).result;
+  // const res = await fetch(`${baseUrl}/api/users`);
+  // const users = (await res.json()).result;
 
   return (
     <List>
-      {users.map((user: User) => {
+      {/* {users.map((user: User) => {
         return <ListItem key={user.id}>{user.firstName}</ListItem>;
-      })}
+      })} */}
     </List>
   );
 }
